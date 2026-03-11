298dc439b63769206cddcd1ed90c652ba2999a35
Zephyrrr <kuangdadu@gmail.com>
2026-03-10 20:08:54 -0700

    feat: 为 robots.txt 和 sitemap.xml 添加 HEAD 请求处理器 / Add HEAD request handlers for robots.txt and sitemap.xml

--- src/routes/sitemap[.]xml.ts
+++ src/routes/sitemap[.]xml.ts
@@ -68,6 +68,14 @@ export const Route = createFileRoute("/sitemap.xml")({
           },
         });
       },
+      HEAD: async () => {
+        return new Response(null, {
+          headers: {
+            "Content-Type": "application/xml; charset=utf-8",
+            "Cache-Control": "public, max-age=3600, s-maxage=3600",
+          },
+        });
+      },
     },
   },
 });
